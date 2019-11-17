10a097780d974a55d5a72a574233ddecea8e669f
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2019-11-17 11:33:10 +0530

    this is next msg on 17/11

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 df = pd.read_excel('sal_exp.xlsx',sheet_name = 'Sheet1')
 X = df[['Experience']]
 #X = df['Experience'].values.reshape(-1,1)
